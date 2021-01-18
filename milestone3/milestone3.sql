7d93a9363ae5be8b545a174ec8e89698cba7516e
ricardo-monreal <moises.monreal@gmail.com>
2021-01-17 17:42:39 -0800

    Update style.css

---
+++ milestone3/milestone3.sql
@@ -0,0 +1,125 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 18, 2021 at 12:12 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `milestone3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blog_posts`
+--
+
+CREATE TABLE `blog_posts` (
+  `idblog_posts` int(11) NOT NULL,
+  `blog_post_title` varchar(45) DEFAULT NULL,
+  `blog_post` text,
+  `users_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `blog_posts`
+--
+
+INSERT INTO `blog_posts` (`idblog_posts`, `blog_post_title`, `blog_post`, `users_ID`) VALUES
+(1, 'Test Post', 'This is to test the database connection. ', 14),
+(2, 'Meridith test', 'test post', 17),
+(3, 'Meridith test', 'test post', 17),
+(4, 'test', 'testing database connection', 22),
+(5, 'test 2', 'testing redirecting to index page', 22),
+(6, 'Test HTML Editor', '<p><strong>Hello!</strong></p>\r\n<p>Testing HTML editor. </p>\r\n<p>&nbsp;</p>', 22),
+(7, 'Dwight Test', '<p>Hello this is a test by Dwight!</p>', 26),
+(8, 'Dwight Test 2', '<p>Whoa!? A second test?</p>', 26);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `FIRST_NAME` varchar(100) NOT NULL,
+  `LAST_NAME` varchar(100) NOT NULL,
+  `USERNAME` varchar(50) NOT NULL,
+  `PASSWORD` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`ID`, `FIRST_NAME`, `LAST_NAME`, `USERNAME`, `PASSWORD`) VALUES
+(14, 'Toby ', 'Flenderson', 'toby', 'pass7'),
+(17, 'Meridith', 'Palmer', 'meridith', 'pass'),
+(21, 'testone', 'test', 'testone', 'pass'),
+(22, 'Darth', 'Vader', 'vader', 'pass'),
+(24, 'Toby', 'Flenderson', 'toby', 'pass'),
+(25, 'Michael', 'Scott', 'michael', 'pass'),
+(26, 'Dwight', 'Schrute', 'dwight', 'pass');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `blog_posts`
+--
+ALTER TABLE `blog_posts`
+  ADD PRIMARY KEY (`idblog_posts`),
+  ADD KEY `fk_blog_posts_users_idx` (`users_ID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `blog_posts`
+--
+ALTER TABLE `blog_posts`
+  MODIFY `idblog_posts` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `blog_posts`
+--
+ALTER TABLE `blog_posts`
+  ADD CONSTRAINT `fk_blog_posts_users` FOREIGN KEY (`users_ID`) REFERENCES `users` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
