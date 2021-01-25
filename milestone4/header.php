b27c9dd625ac9a98d36b9851f3f8387b3e5ea3a1
ricardo-monreal <moises.monreal@gmail.com>
2021-01-24 18:04:23 -0800

    Update header.php

--- milestone4/header.php
+++ milestone4/header.php
@@ -4,7 +4,7 @@
  * Date: January 24, 2021
  -->
 <head>
-    <title>CST-126 Milestone 3</title>
+    <title>CST-126 Milestone 4</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/style.css" media="all">
 </head>
