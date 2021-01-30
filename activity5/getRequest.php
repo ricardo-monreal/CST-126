4c22edb32320441cc041521c0c13ce85a2a31f3f
ricardo-monreal <moises.monreal@gmail.com>
2021-01-30 13:13:39 -0700

    Activity 5

---
+++ activity5/getRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+	$id = $_GET['id'];
+	$mode = $_GET['mode'];
+
+	echo "ID: " . $id;
+	echo "<br>";
+	echo "Mode: " . $mode;
+    echo "<br>";
+
+	if( $_GET["name"] || $_GET["email"] || $_GET["contact"])
+	{
+		echo "Welcome: ". $_GET['name']. "<br />";
+		echo "Your Email is: ". $_GET["email"]. "<br />";
+		echo "Your Blog Post: " . htmlspecialchars($_GET['blog_post']);
+		}
